d73ba271ddfd411e77c0f5ae001306bca98315d9
Caleb Barde <caleb@bonus.ly>
2018-12-04 12:49:51 -0700

    janky ass solution

---
+++ day_1_part_2.exs
@@ -0,0 +1,19 @@
+File.stream!("input/day_1.txt")
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(&String.to_integer/1)
+  |> Stream.cycle()
+  |> Stream.scan(&+/2)
+  |> Stream.scan(MapSet.new(), fn item, acc ->
+    if MapSet.member?(acc, item) do
+      item
+    else
+      MapSet.put(acc, item)
+    end
+  end)
+  |> Stream.drop_while(&(!is_integer(&1)))
+  |> Enum.to_list
+  |> List.first
+  |> Stream.run
+  |> IO.puts
+
+
